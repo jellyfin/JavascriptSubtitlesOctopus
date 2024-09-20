e6b22202d1c8f6e02e0e2b6076c1ce4af939ea6d
Dmitry Lyzo <ashephard0@gmail.com>
2024-09-21 02:31:54 +0300

    Bump to 4.2.2 (Jellyfin)

--- assets/js/subtitles-octopus.js
+++ assets/js/subtitles-octopus.js
@@ -303,23 +303,32 @@
 //   2014 Google, Inc.
 // 
 // # Project: libass
+// License: BSD-2-clause
+// Copyright: 2018 Two Orioles, LLC
+//   2018 VideoLAN and dav1d authors
+// 
 // License: ISC
 // Copyright: 2006 Evgeniy Stepanov <eugeni.stepanov@gmail.com>
 //   2006-2016 libass contributors
 //   2009 Grigori Goronzy <greg@geekmind.org>
+//   2009-2022 libass contributors
 //   2010 Grigori Goronzy <greg@geekmind.org>
 //   2011 Grigori Goronzy <greg@chown.ath.cx>
 //   2011-2014 Yu Zhuohuang <yuzhuohuang@qq.com>
 //   2013 Stefano Pigozzi <stefano.pigozzi@gmail.com>
 //   2013 rcombs <rcombs@rcombs.me>
 //   2014 Vabishchevich Nikolay <vabnick@gmail.com>
+//   2014-2022 libass contributors
 //   2015 Grigori Goronzy <greg@kinoho.net>
 //   2015 Oleg Oshmyan <chortos@inbox.lv>
 //   2015 Stephan Vedder <stephan.vedder@gmail.com>
 //   2015 Vabishchevich Nikolay <vabnick@gmail.com>
+//   2015-2022 libass contributors
 //   2016 Vabishchevich Nikolay <vabnick@gmail.com>
 //   2017 Vabishchevich Nikolay <vabnick@gmail.com>
+//   2020-2022 libass contributors
 //   2021 libass contributors
+//   2021-2022 libass contributors
 //   2022 libass contributors
 //   interest in the
 // 
@@ -343,6 +352,29 @@
 // #   MIT~old        | MIT-Modern-Variant
 // #   Expat          | MIT
 // #   NTP~disclaimer | (None; it is NTP with a more verbose warranty disclaimer)
+// 
+// License: BSD-2-clause
+//   Redistribution and use in source and binary forms, with or without
+//   modification, are permitted provided that the following conditions are met:
+//   .
+//   1. Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//   .
+//   2. Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//   .
+//   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+//   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+//   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//   POSSIBILITY OF SUCH DAMAGE.
 // 
 // License: Expat
 //   Permission is  hereby granted,  free of charge,  to any  person obtaining
